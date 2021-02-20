3505a7266caae1ab9fec75b3af69d2f16cd89daa
Bilal Ahmed <bilal91_@hotmail.com>
2021-02-20 18:26:18 +0000

    fixed questions

--- src/App.js
+++ src/App.js
@@ -1,98 +1,98 @@
-import React, {useState} from "react"; 
+import React, { useState } from "react";
 
-
-import "./App.css"
-
+import "./App.css";
 
 export default function App() {
   const questions = [
-		{
-			questionText: 'What is my name?',
-			answerOptions: [
-				{ answerText: 'Abdallah', isCorrect: false },
-				{ answerText: 'Mohammed', isCorrect: false },
-				{ answerText: 'Ali', isCorrect: true },
-				{ answerText: 'John', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'Where do i live?',
-			answerOptions: [
-				{ answerText: 'Belarus', isCorrect: false },
-				{ answerText: 'Tokyo', isCorrect: true },
-				{ answerText: 'New York', isCorrect: false },
-				{ answerText: 'Nottingham', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'How did you came to know?',
-			answerOptions: [
-				{ answerText: 'Abu Muhammed said', isCorrect: true },
-				{ answerText: 'Elmi did the tutsy roll', isCorrect: false },
-				{ answerText: 'Wiki answers', isCorrect: false },
-				{ answerText: 'I heard her say', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'How many Harry Potter books are there?',
-			answerOptions: [
-				{ answerText: '1', isCorrect: false },
-				{ answerText: '4', isCorrect: false },
-				{ answerText: '6', isCorrect: false },
-				{ answerText: '7', isCorrect: true },
-			],
-		},
+    {
+      questionText: "What is my name?",
+      answerOptions: [
+        { answerText: "Abdallah", isCorrect: false },
+        { answerText: "Mohammed", isCorrect: false },
+        { answerText: "Bilal", isCorrect: true },
+        { answerText: "John", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "Where do i live?",
+      answerOptions: [
+        { answerText: "Belarus", isCorrect: false },
+        { answerText: "Tokyo", isCorrect: true },
+        { answerText: "New York", isCorrect: false },
+        { answerText: "Nottingham", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "How old am I ?",
+      answerOptions: [
+        { answerText: 29, isCorrect: true },
+        { answerText: 14, isCorrect: false },
+        { answerText: 21, isCorrect: false },
+        { answerText: 28, isCorrect: false },
+      ],
+    },
+    {
+      questionText: "How many Harry Potter books are there?",
+      answerOptions: [
+        { answerText: "1", isCorrect: false },
+        { answerText: "4", isCorrect: false },
+        { answerText: "6", isCorrect: false },
+        { answerText: "7", isCorrect: true },
+      ],
+    },
   ];
 
- 
-  
-  const[currentQuestion, setCurrentQuestion] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
   // used to display the score pannel at the end
-  const [showScore, setShowScore]= useState(false)
+  const [showScore, setShowScore] = useState(false);
 
-  const [score, setScore] = useState(0)
+  const [score, setScore] = useState(0);
   //  change the question and if someone reaches end of quiz, show the score pannel
-  const handleButtonClick =  (isCorrect) => {
+  const handleButtonClick = (isCorrect) => {
     // adds up the correct answers
-    if(isCorrect===true){
-      
-      setScore(score+1)
+    if (isCorrect === true) {
+      setScore(score + 1);
     }
 
     const nextQuestion = currentQuestion + 1;
-    if (nextQuestion<questions.length){
-      setCurrentQuestion(nextQuestion) ;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
     } else {
-      setShowScore(true)
+      setShowScore(true);
     }
-    
-  }
-	return (
-		<div className='app'>
-			
-			{showScore ? (
-				<div className='score-section'>
+  };
+  return (
+    <div className="app">
+      {showScore ? (
+        <div className="score-section">
           <p>Congratulations!</p>
           <br></br>
-          <p>You scored {score} out of {questions.length}</p></div>
-			) : (
-				<>
-					<div className='question-section'>
+          <p>
+            You scored {score} out of {questions.length}
+          </p>
+        </div>
+      ) : (
+        <>
+          <div className="question-section">
+            <div className="question-text">
+              <h1>{questions[currentQuestion].questionText}</h1>
+            </div>
 
-            <div className='question-text'><h1>{questions[currentQuestion].questionText}</h1></div>
-
-						<div className='question-count'>
-      <h2>Question {currentQuestion + 1} /{questions.length}</h2>
-						</div>
-      
-					</div>
-					<div className='answer-section'>
-            {questions[currentQuestion].answerOptions.map((answerOption)=><button onClick = {()=>handleButtonClick(answerOption.isCorrect)}>{answerOption.answerText}</button>)}
-					</div>
-				</>
-			)}
-		</div>
-	);
-
-
-};
+            <div className="question-count">
+              <h2>
+                Question {currentQuestion + 1} /{questions.length}
+              </h2>
+            </div>
+          </div>
+          <div className="answer-section">
+            {questions[currentQuestion].answerOptions.map((answerOption) => (
+              <button onClick={() => handleButtonClick(answerOption.isCorrect)}>
+                {answerOption.answerText}
+              </button>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
